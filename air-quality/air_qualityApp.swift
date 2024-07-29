7b0a9ee4ecdd405447b7be2f810c7405989b2176
Rory Flint <rory.flint@tipalti.com>
2024-07-29 20:27:49 +0100

    Initial Commit

---
+++ air-quality/air_qualityApp.swift
@@ -0,0 +1,17 @@
+//
+//  air_qualityApp.swift
+//  air-quality
+//
+//  Created by Rory Flint on 29/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct air_qualityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
